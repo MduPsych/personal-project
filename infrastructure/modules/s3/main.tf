a0b4e1cd0364da69345c2e9e4e5c2b932d0cdfeb
Mduduzi.Msiza <mduduzi.msiza@shoprite.co.za>
2026-06-02 11:40:39 +0200

    Add delete marker replication to S3 replication config

--- infrastructure/modules/s3/main.tf
+++ infrastructure/modules/s3/main.tf
@@ -112,6 +112,10 @@ resource "aws_s3_bucket_replication_configuration" "email_bucket" {
 
     filter {}
 
+    delete_marker_replication {
+      status = "Enabled"
+    }
+
     destination {
       bucket        = "arn:aws:s3:::${var.environment}-${var.project_name}-emails-replica-${var.aws_account_id}"
       storage_class = "STANDARD_IA"
